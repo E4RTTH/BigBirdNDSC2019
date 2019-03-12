48de89efdd8d1891bbe87b217720d5aad9b5be0b
Jia Yuan Chia <jiayuan.chia@besi.com>
2019-03-12 09:17:09 +0800

    Fix translation script

--- translateBatchToENG.py
+++ translateBatchToENG.py
@@ -1,53 +1,57 @@
 from translate_selenium import Translate, Translate_Concurrent
 import pandas as pd
 
+translator = Translate(from_lang = 'id', to_lang = 'en')
+
 dataset = pd.read_csv('mobile_data_info_train_competition.csv', quoting = 3)
 titles = dataset['title'].values
-translators = Translate_Concurrent(batch_size = len(titles), from_lang = 'ms', to_lang = 'en')
-titles_eng = translators.translate_batch(titles)
+titles_eng = [translator.translate(title) for title in titles]
 dataset['title_eng'] = titles_eng
 dataset.to_csv('mobile_data_info_train_competition_eng.csv', index=False)
 
+del dataset, titles, titles_eng
+
 dataset = pd.read_csv('mobile_data_info_val_competition.csv', quoting = 3)
 titles = dataset['title'].values
-translators = Translate_Concurrent(batch_size = len(titles), from_lang = 'ms', to_lang = 'en')
-titles_eng = translators.translate_batch(titles)
+titles_eng = [translator.translate(title) for title in titles]
 dataset['title_eng'] = titles_eng
 dataset.to_csv('mobile_data_info_val_competition_eng.csv', index=False)
 
 del dataset, titles, titles_eng
 
 
+
+
 dataset = pd.read_csv('beauty_data_info_train_competition.csv', quoting = 3)
 titles = dataset['title'].values
-translators = Translate_Concurrent(batch_size = len(titles), from_lang = 'ms', to_lang = 'en')
-titles_eng = translators.translate_batch(titles)
+titles_eng = [translator.translate(title) for title in titles]
 dataset['title_eng'] = titles_eng
-dataset.to_csv('beauty_data_info_train_competition.csv', index=False)
+dataset.to_csv('beauty_data_info_train_competition_eng.csv', index=False)
+
+del dataset, titles, titles_eng
 
 dataset = pd.read_csv('beauty_data_info_val_competition.csv', quoting = 3)
 titles = dataset['title'].values
-translators = Translate_Concurrent(batch_size = len(titles), from_lang = 'ms', to_lang = 'en')
-titles_eng = translators.translate_batch(titles)
+titles_eng = [translator.translate(title) for title in titles]
 dataset['title_eng'] = titles_eng
-dataset.to_csv('beauty_data_info_val_competition.csv', index=False)
+dataset.to_csv('beauty_data_info_val_competition_eng.csv', index=False)
 
 del dataset, titles, titles_eng
 
 
 dataset = pd.read_csv('fashion_data_info_train_competition.csv', quoting = 3)
 titles = dataset['title'].values
-translators = Translate_Concurrent(batch_size = len(titles), from_lang = 'ms', to_lang = 'en')
-titles_eng = translators.translate_batch(titles)
+titles_eng = [translator.translate(title) for title in titles]
 dataset['title_eng'] = titles_eng
-dataset.to_csv('fashion_data_info_train_competition.csv', index=False)
+dataset.to_csv('fashion_data_info_train_competition_eng.csv', index=False)
+
+del dataset, titles, titles_eng
 
 dataset = pd.read_csv('fashion_data_info_val_competition.csv', quoting = 3)
 titles = dataset['title'].values
-translators = Translate_Concurrent(batch_size = len(titles), from_lang = 'ms', to_lang = 'en')
-titles_eng = translators.translate_batch(titles)
+titles_eng = [translator.translate(title) for title in titles]
 dataset['title_eng'] = titles_eng
-dataset.to_csv('fashion_data_info_val_competition.csv', index=False)
+dataset.to_csv('fashion_data_info_val_competition_eng.csv', index=False)
 
 del dataset, titles, titles_eng
 
