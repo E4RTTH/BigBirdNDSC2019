8f26564e3bd4d37ca9d5daf7d4e6395f317a43ac
jychia <jiayuan.chia@outlook.com>
2019-03-14 00:09:20 +0800

    Update testtest.py

--- testtest.py
+++ testtest.py
@@ -18,8 +18,21 @@ from sklearn.linear_model.stochastic_gradient import SGDClassifier
 def preprocess_data(titles, regex):
     ps = PorterStemmer()
     data = []
-    for i in range(0, len(titles)):
-        title = re.sub(regex, ' ', titles[i])
+    for item in titles:
+        
+        title = item
+        
+        # Remove all the spaces between numbers and keyterm
+        title = re.sub('(?<=(\d)) (?=(g ))', '', title) 
+        title = re.sub('(?<=(\d)) (?=(gb|mb))', '', title)
+        title = re.sub('(?<=(\d)) (?=(mp))', '', title)   
+        title = re.sub('tahun|thn|yr', 'year', title)   
+        title = re.sub('bulan|bln|mth', 'month', title)
+        title = re.sub('(?<=(\d)) (?=(year|month|))', '', title)
+        title = re.sub('(?<=(\d)) (?=(inch))', '', item) 
+        title = re.sub('[\S]*(gb|mb|mp|year|month)', '', item) 
+        
+        title = re.sub(regex, ' ', item)
         title = title.lower()
         title = title.split()
         title = [ps.stem(word) for word in title if not word in set(stopwords.words('english'))]
@@ -44,11 +57,11 @@ dataset = pd.read_csv('mobile_data_info_train_competition.csv', quoting = 3)
 # Update stopwords database
 nltk.download('stopwords')
 
-classifier = RandomForestClassifier(n_estimators = 300, criterion = 'entropy', random_state = 0, max_depth=100)
-#SGDClassifier(alpha=0.0001, penalty='elasticnet')
+classifier = RandomForestClassifier(n_estimators = 300, criterion = 'entropy', random_state = 0, min_samples_split = 6)
+#, max_depth=130
 
-attr_name = 'Brand'
-regex = '[^a-zA-Z0-9]'
+attr_name = 'Phone Screen Size'
+regex = '[^a-zA-Z0-9\.]'
 
 # Some declaration and initialization
 X = []
@@ -90,40 +103,3 @@ df_wrong = df[df.actual != df.pred]
 
 cm = confusion_matrix(y_test, y_pred)
 
-
-
-"""
-acc_OS, f1_OS = train_predict_data(dataset, 'Operating System', classifier, '[^a-zA-Z]')
-print("OS: acc=", acc_OS, ", f1=", f1_OS)
-
-acc_Features, f1_Features, acc_Features_train, f1_Features_train, df_Features = train_predict_data(dataset, 'Features', classifier, '[^a-zA-Z0-9]')
-print("Features: acc=", acc_Features, ", f1=", f1_Features)
-
-acc_Network, f1_Network = train_predict_data(dataset, 'Network Connections', classifier, '[^a-zA-Z0-9]')
-print("Network Connections: acc=", acc_Network, ", f1=", f1_Network)
-
-acc_RAM, f1_RAM = train_predict_data(dataset, 'Memory RAM', classifier, '[^a-zA-Z0-9]')
-print("Memory RAM: acc=", acc_RAM, ", f1=", f1_RAM)
-
-acc_Brand, f1_Brand = train_predict_data(dataset, 'Brand', classifier, '[^a-zA-Z]')
-print("Brand: acc=", acc_Brand, ", f1=", f1_Brand)
-
-acc_Warranty, f1_Warranty = train_predict_data(dataset, 'Warranty Period', classifier, '[^a-zA-Z0-9]')
-print("Warranty Period: acc=", acc_Warranty, ", f1=", f1_Warranty)
-
-acc_Storage, f1_Storage = train_predict_data(dataset, 'Storage Capacity', classifier, '[^a-zA-Z0-9]')
-print("Storage Capacity: acc=", acc_Storage, ", f1=", f1_Storage)
-
-acc_Color, f1_Color = train_predict_data(dataset, 'Color Family', classifier, '[^a-zA-Z]')
-print("Color Family: acc=", acc_Color, ", f1=", f1_Color)
-
-acc_Model, f1_Model = train_predict_data(dataset, 'Phone Model', classifier, '[^a-zA-Z0-9]')
-print("Phone Model: acc=", acc_Model, ", f1=", f1_Model)
-
-acc_Camera, f1_Camera = train_predict_data(dataset, 'Camera', classifier, '[^a-zA-Z0-9]')
-print("Camera: acc=", acc_Camera, ", f1=", f1_Camera)
-
-acc_Size, f1_Size = train_predict_data(dataset, 'Phone Screen Size', classifier, '[^a-zA-Z0-9]')
-print("Phone Screen Size: acc=", acc_Size, ", f1=", f1_Size)
-
-"""
